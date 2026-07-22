f348b013d48394d88b0201c743d5dd49aebf2345
phuongtech24 <namphuong2462@gmail.com>
2026-07-22 22:47:40 +0700

    feat: áp dụng thêm mẫu thiết kế

--- core-banking/src/main/java/com/tplite/core_banking/module/auth/config/PasswordConfig.java
+++ core-banking/src/main/java/com/tplite/core_banking/module/auth/config/PasswordConfig.java
@@ -1,14 +1,19 @@
 package com.tplite.core_banking.module.auth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.tplite.core_banking.module.auth.factory.PasswordEncoderFactory;
+import com.tplite.core_banking.module.auth.factory.PasswordEncoderType;
 
 @Configuration
 public class PasswordConfig {
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public PasswordEncoder passwordEncoder(
+            @Value("${app.security.password.encoder:BCRYPT}") PasswordEncoderType encoderType
+    ) {
+        return PasswordEncoderFactory.create(encoderType);
     }
 }
